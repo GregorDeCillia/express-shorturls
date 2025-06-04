b5a02ef4073b1172948c3679a8f3c0be6b832e39
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-04 22:29:31 +0200

    first feature-complete version

--- api/index.js
+++ api/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true })); 
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const urls = [];
+const addUrl = url => urls.push(url) - 1;
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
 });
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  if (! /^http/.test(url)) {
+    res.json({ error: 'invalid url' })
+  } else {
+    res.json({ original_url: url, short_url: addUrl(url) })
+  }
+})
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const url = urls[id];
+  if (url) {
+    res.redirect(url)
+  } else {
+    res.json({ error: 'invalid short url' })
+  }
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(process.cwd() + '/index.html');
+})
+
+app.use(express.static('public'));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
